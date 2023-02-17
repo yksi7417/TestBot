61a017b4e072282485d6ab64fc6d12ec09b3005a
Anthony Si <yatkasi@gmail.com>
2023-02-16 23:16:28 -0500

    logging.ini not being used, but using Application.h make program simplier

--- md5.cpp
+++ md5.cpp
@@ -1,35 +1,28 @@
 #include <Poco/MD5Engine.h>
 #include <Poco/DigestStream.h>
+#include <Poco/Util/Application.h>
 #include <Poco/Logger.h>
-#include <Poco/ConsoleChannel.h>
-#include <Poco/FormattingChannel.h>
-#include <Poco/PatternFormatter.h>
-
-#include <Poco/AutoPtr.h>
-#include <Poco/Util/IniFileConfiguration.h>
-#include <Poco/Util/LoggingConfigurator.h>
-
 #include <iostream>
 
-int main(int argc, char** argv){
-    Poco::Logger& logger = Poco::Logger::get("Logger");
-    Poco::AutoPtr<Poco::ConsoleChannel> pChannel(new Poco::ConsoleChannel);
-    Poco::AutoPtr<Poco::PatternFormatter> pPF(new Poco::PatternFormatter);
-    pPF->setProperty("pattern", "%Y-%m-%d %H:%M:%S.%i [%s] [%p] %t");
-    Poco::AutoPtr<Poco::FormattingChannel> pFC(new Poco::FormattingChannel(pPF, pChannel));
-    logger.setChannel(pFC);
+class MyApplication : public Poco::Util::Application
+{
+public:
+    int main(const std::vector<std::string>& args)
+    {
+        Poco::MD5Engine md5;
+        Poco::DigestOutputStream ds(md5);
+        ds << "abcdefghijklmnopqrstuvwxyz";
+        ds.close();
 
-    Poco::AutoPtr<Poco::Util::IniFileConfiguration> pConfig = new Poco::Util::IniFileConfiguration("logging.ini");
-    Poco::Util::LoggingConfigurator configurator;
-    configurator.configure(pConfig);
+        poco_information(logger(), Poco::DigestEngine::digestToHex(md5.digest()) );
+        poco_information(logger(), "hello simplier !" );
 
+        return Application::EXIT_OK;
+    }
+};
 
-    Poco::MD5Engine md5;
-    Poco::DigestOutputStream ds(md5);
-    ds << "abcdefghijklmnopqrstuvwxyz";
-    ds.close();
-    std::cout << Poco::DigestEngine::digestToHex(md5.digest()) << std::endl;
-
-    poco_information(logger, "Hello, POCO!");
-    return 0;
+int main(int argc, char** argv)
+{
+    MyApplication app;
+    return app.run();
 }
