7e028b8330759b4f15b28556f08b6235f473242e
Anthony Si <yatkasi@gmail.com>
2023-02-16 22:39:42 -0500

    forced logging

--- md5.cpp
+++ md5.cpp
@@ -1,6 +1,9 @@
 #include <Poco/MD5Engine.h>
 #include <Poco/DigestStream.h>
 #include <Poco/Logger.h>
+#include <Poco/ConsoleChannel.h>
+#include <Poco/FormattingChannel.h>
+#include <Poco/PatternFormatter.h>
 
 #include <Poco/AutoPtr.h>
 #include <Poco/Util/IniFileConfiguration.h>
@@ -8,36 +11,18 @@
 
 #include <iostream>
 
-int configureLogger() {
-    try {
-        Poco::AutoPtr<Poco::Util::IniFileConfiguration> pConfig = new Poco::Util::IniFileConfiguration("logging.ini");
-        Poco::Util::LoggingConfigurator configurator;
-        configurator.configure(pConfig);
-    }
-    catch (std::exception& ex) {
-        // handle specific exceptions
-        std::cerr << __FILE__ << ":" << __FUNCTION__  << " Exception caught: " << ex.what() << std::endl;
-        return -1; 
-    }
-    catch (...) {
-        // catch all other exceptions
-        std::cerr << __FILE__ << ":" << __FUNCTION__  << " Unknown exception caught" << std::endl;
-        return -1; 
-    }
-
-    std::cout << __FILE__ << ":" << __FUNCTION__ << " Successful!!" << std::endl;
-    return 0; 
-}
-
 int main(int argc, char** argv){
-    Poco::Logger& logger = Poco::Logger::get("MyLogger");
+    Poco::Logger& logger = Poco::Logger::get("Logger");
+    Poco::AutoPtr<Poco::ConsoleChannel> pChannel(new Poco::ConsoleChannel);
+    Poco::AutoPtr<Poco::PatternFormatter> pPF(new Poco::PatternFormatter);
+    pPF->setProperty("pattern", "%Y-%m-%d %H:%M:%S.%i [%s] [%p] %t");
+    Poco::AutoPtr<Poco::FormattingChannel> pFC(new Poco::FormattingChannel(pPF, pChannel));
+    logger.setChannel(pFC);
+
     Poco::AutoPtr<Poco::Util::IniFileConfiguration> pConfig = new Poco::Util::IniFileConfiguration("logging.ini");
     Poco::Util::LoggingConfigurator configurator;
     configurator.configure(pConfig);
-    logger.setLevel("information");
-    logger.error("Hello, POCO!");
 
-    configureLogger();
 
     Poco::MD5Engine md5;
     Poco::DigestOutputStream ds(md5);
@@ -45,6 +30,6 @@ int main(int argc, char** argv){
     ds.close();
     std::cout << Poco::DigestEngine::digestToHex(md5.digest()) << std::endl;
 
-    logger.warning("Hello, POCO!");
+    logger.information("Hello, POCO!");
     return 0;
 }
