bcfd50b4ef25681a6b6933f16fd5350c97213573
Anthony Si <yatkasi@gmail.com>
2023-02-25 22:34:12 -0500

    linking the library with test cases

--- tests/md5_plugin/md5_plugin_test.cpp
+++ tests/md5_plugin/md5_plugin_test.cpp
@@ -1,9 +1,7 @@
 #include "gtest/gtest.h"
+#include "md5_plugin/md5.h"
 
-TEST(TestMd5Case, TestOne) {
-    EXPECT_EQ(1, 1);
-}
-
-TEST(TestMd5Case, TestTwo) {
-    EXPECT_NE(1, 2);
+TEST(TestMd5Case, GetResult) {
+    Md5Plugin md5_plugin; 
+    EXPECT_EQ("c3fcd3d76192e4007dfb496cca67e13b", md5_plugin.getResult("abcdefghijklmnopqrstuvwxyz"));
 }
